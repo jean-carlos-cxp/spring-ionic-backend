b27e2fbe713e7ab8aecbaf9c34a7935a006e61a5
Jean Carlos <carlosjean090@gmail.com>
2021-06-02 14:13:41 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/jeancarlos/cursomc/CursoModelagemConceitualApplication.java
+++ src/main/java/com/jeancarlos/cursomc/CursoModelagemConceitualApplication.java
@@ -1,5 +1,6 @@
 package com.jeancarlos.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.jeancarlos.cursomc.domain.Cidade;
 import com.jeancarlos.cursomc.domain.Cliente;
 import com.jeancarlos.cursomc.domain.Endereco;
 import com.jeancarlos.cursomc.domain.Estado;
+import com.jeancarlos.cursomc.domain.Pagamento;
+import com.jeancarlos.cursomc.domain.PagamentoComBoleto;
+import com.jeancarlos.cursomc.domain.PagamentoComCartao;
+import com.jeancarlos.cursomc.domain.Pedido;
 import com.jeancarlos.cursomc.domain.Produto;
+import com.jeancarlos.cursomc.domain.enums.EstadoPagamento;
 import com.jeancarlos.cursomc.domain.enums.TipoCliente;
 import com.jeancarlos.cursomc.repositories.CategoriaRepository;
 import com.jeancarlos.cursomc.repositories.CidadeRepository;
 import com.jeancarlos.cursomc.repositories.ClienteRepository;
 import com.jeancarlos.cursomc.repositories.EnderecoRepository;
 import com.jeancarlos.cursomc.repositories.EstadoRepository;
+import com.jeancarlos.cursomc.repositories.PagamentoRepository;
+import com.jeancarlos.cursomc.repositories.PedidoRepository;
 import com.jeancarlos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -41,6 +49,12 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository; 
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoModelagemConceitualApplication.class, args);
@@ -87,7 +101,22 @@ public class CursoModelagemConceitualApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(end1, end2));
-	
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, end1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, end2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 	
 }
---
+++ src/main/java/com/jeancarlos/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,31 @@
+package com.jeancarlos.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.jeancarlos.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer qtdParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer qtdParcelas) {
+		super(id, estado, pedido);
+		this.qtdParcelas = qtdParcelas;
+	}
+
+	public Integer getQtdParcelas() {
+		return qtdParcelas;
+	}
+
+	public void setQtdParcelas(Integer qtdParcelas) {
+		this.qtdParcelas = qtdParcelas;
+	}
+	
+}
