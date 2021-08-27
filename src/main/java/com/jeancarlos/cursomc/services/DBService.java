2159157b913e8d42adfac3a819919b81c09afa30
Jean Carlos <jean.silva0096@gmail.com>
2021-08-27 16:24:13 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/jeancarlos/cursomc/services/DBService.java
+++ src/main/java/com/jeancarlos/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.jeancarlos.cursomc.domain.PagamentoComCartao;
 import com.jeancarlos.cursomc.domain.Pedido;
 import com.jeancarlos.cursomc.domain.Produto;
 import com.jeancarlos.cursomc.domain.enums.EstadoPagamento;
+import com.jeancarlos.cursomc.domain.enums.Perfil;
 import com.jeancarlos.cursomc.domain.enums.TipoCliente;
 import com.jeancarlos.cursomc.repositories.CategoriaRepository;
 import com.jeancarlos.cursomc.repositories.CidadeRepository;
@@ -122,16 +123,21 @@ public class DBService {
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "jean.silva0096@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
-
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "carlosjean090@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+		cli2.addPerfil(Perfil.ADMIN);
 
 		Endereco end1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardie", "38220834", cli1, c1);
 		Endereco end2 = new Endereco(null, "Avenida Matos", "105", "Apto 800", "Centro", "38777012", cli1, c2);
-
+		Endereco end3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "281777012", cli2, c2);
+		
 		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
-
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(end1, end2));
+		cli2.getEnderecos().addAll(Arrays.asList(end3));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(end1, end2, end3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 
