91887a3b788bf13a98357787051fd04ec4937678
Jean Carlos <jean.silva0096@gmail.com>
2021-08-28 17:24:30 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/jeancarlos/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/jeancarlos/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.jeancarlos.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jeancarlos.cursomc.domain.Cliente;
 import com.jeancarlos.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
+	@Transactional
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
